ba2aa63b009aed043cfabe2612f05c65d7a935af
samirsaiyed256 <samirsaiyed256@gmail.com>
2025-07-09 06:29:28 +0000

    Related to data add little-bit code

---
+++ data_ingestion/data_ingest.py
@@ -0,0 +1,16 @@
+from langchain_astradb import AstraDBVectorStore
+from dotenv import load_dotenv
+import os
+import pandas as pd
+
+from data_ingestion.data_transform import data_convert
+
+
+class ingest_data:
+
+    def __init__(self):
+        pass
+
+    def data_ingestion(self):
+        pass
+    
---
+++ data_ingestion/data_transform.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from langchain_core.documents import Document
+
+
+class data_convert:
+    def __init__(self):
+        pass
+
+    def data_transformation(self):
+        pass
