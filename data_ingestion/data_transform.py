3b06bcd7968c87fc8d76aaabc480cacfeb6d622d
samirsaiyed256 <samirsaiyed256@gmail.com>
2025-07-09 07:03:41 +0000

    Data loaded and read

--- data_ingestion/data_transform.py
+++ data_ingestion/data_transform.py
@@ -5,6 +5,9 @@ from langchain_core.documents import Document
 class data_convert:
     def __init__(self):
         print("Data transformation class has initialized")
+        
+        self.product_data = pd.read_csv(r"data/flipkart_product_review.csv")
+        print(self.product_data.head())
 
     def data_transformation(self):
         pass
